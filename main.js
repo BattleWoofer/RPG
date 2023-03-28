f9070e4d87b26f45711a3a6d57e4f43e58766036
BattleWoofer <omeet100@gmail.com>
2023-03-27 22:31:34 -0500

    fs

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var gold = 0;
+var AD = 1;
+var attackSeconds = 3;
+
+var zone = 1;
+var enemyMaxHp = 5;
+var enemyHp = 5;
+
+loadZone(0);
+function loadZone(up){
+    zone += up;
+    enemyMaxHp = Math.floor(5 * Math.pow(1.25, zone - 1));
+    enemyHp = enemyMaxHp;
+    document.getElementById("zone").innerHTML = "Zone " + zone;
+}
+
 var lastFrameAt = 0;
 function Update(timestamp){;
     delta = timestamp - lastFrameAt;
@@ -8,11 +24,14 @@ function Update(timestamp){;
     delta = delta / 1000
     Charge(delta);
     barUpdate();
+
+    ADCalc();
+    document.getElementById("gold").innerHTML = "Gold: " + gold;
+
 }
 window.requestAnimationFrame(Update)
 
 var attackCharge = 0;
-var attackSeconds = 4;
 function Charge(time){
     attackCharge += time;
     if(attackCharge >= attackSeconds){
@@ -22,14 +41,40 @@ function Charge(time){
     }
 }
 
-var dox = document.getElementById("what")
 
-var AD = 1;
+document.getElementById("treepanel").style.display = "none"
+document.getElementById("upgtab").addEventListener("click", function(a){
+    document.getElementById("upgpanel").style.display = "block"
+    document.getElementById("treepanel").style.display = "none"
+})
+document.getElementById("treetab").addEventListener("click", function(b){
+    document.getElementById("treepanel").style.display = "block"
+    document.getElementById("upgpanel").style.display = "none"
+
+})
+
 function Attack(){
     enemyHp -= AD
     console.log(enemyHp + AD, enemyHp)
+    if(enemyHp <= 0){kill()}
 }
-
+function kill(){
+    gold  += Math.floor((2 * Math.pow(1.2, zone - 1)) + (zone - 1))
+    loadZone(1)
+}
+document.getElementById("sword").addEventListener("click", function(buyS){
+    buySword()
+})
+ADPurchase = 0;
+function buySword(){
+    if(gold >= Math.floor(4 * (Math.pow(1.5, ADPurchase)))){
+    gold -= Math.floor(4 * (Math.pow(1.5, ADPurchase)))
+    ADPurchase += 1;
+    document.getElementById("sword").innerHTML = "Cost: " + Math.floor(4 * (Math.pow(1.5, ADPurchase)))}
+}
+function ADCalc(){
+    AD = 1 + ADPurchase;
+}
 function barUpdate(){
     hpBar = document.getElementById("pp")
     hpText = ["):             "]
@@ -38,5 +83,3 @@ function barUpdate(){
     }
     hpBar.innerHTML = hpText.join("");
 }
-var enemyMaxHp = 5;
-var enemyHp = 5;
