d4412f3b75a6e2ab98e9e9d47e46a46bc4340c46
BattleWoofer <omeet100@gmail.com>
2023-04-05 13:32:15 -0500

    .

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var AD = 1;
 var attackSeconds = 3;
 
 var zone = 1;
+var hZE = 0;
 var enemyMaxHp = 5;
 var enemyHp = 5;
 
@@ -12,12 +13,25 @@ var level = 1;
 var exp = 0;
 var expToNext = 6;
 
+var devspeed = 1;
+var elapsed = 0;
+
+var D11 = false;
+var D12 = false;
+
+var tomes = 0;
+advancing = true;
+
+
 loadZone(0);
 function loadZone(up){
+    playerCurrentHp = playerHp;
     zone += up;
+    if(zone > hZE){hze=zone}
     enemyMaxHp = Math.floor(5 * Math.pow(1.25, zone - 1));
     enemyHp = enemyMaxHp;
-    document.getElementById("zone").innerHTML = "Zone " + zone;
+    valueCalc();
+    document.getElementById("zone").innerHTML = "Zone " + zone + " ("+value + ")";
 }
 hpBar = document.getElementById("playerhpbar")
 function hpBar(){
@@ -27,8 +41,9 @@ function hpBar(){
 var lastFrameAt = 0;
 function Update(timestamp){;
     delta = timestamp - lastFrameAt;
-    if(delta > 20){
-    console.log(delta + "ms")}
+    delta *= devspeed;
+    elapsed += delta / 1000
+    document.getElementById("l").innerHTML = elapsed / 60 + "min"
     window.requestAnimationFrame(Update)
     lastFrameAt = timestamp;
     delta = delta / 1000
@@ -45,10 +60,10 @@ window.requestAnimationFrame(Update)
 var attackCharge = 0;
 function Charge(time){
     attackCharge += time;
-    attackSeconds = 0.1 * Math.pow(0.99, level)
+    attackSeconds = 3 * Math.pow(0.99, level)
     if(attackCharge >= attackSeconds){
         Attack()
-        attackCharge = 0;
+        attackCharge -= attackSeconds;
         return
     }
 }
@@ -61,28 +76,40 @@ function enemyCharges(time){
         return
     }
 }
+function eADCalc(){
+    eAD = (Math.pow(1.1, zone) * (zone * 0.5)) + 1;
+    if(D12 == true){
+        eAD /= (Math.pow(AD, 0.3333333) / 2)
+    }
+    eAD = Math.floor(eAD)
+}
 function enemyAttack(){
-    playerCurrentHp -= Math.floor(Math.pow(1.1, zone) * (zone * 0.5)) + 1
+    eADCalc()
+    playerCurrentHp -= eAD
     if(playerCurrentHp <= 0){
         Die();
     }
 }
 function Die(){
     if(zone > 5){
-        loadZone(-5)
+        loadZone(-1)
     }else{
         loadZone((-1 * zone)+1)
     }
     playerLoadHp(1);
+    advancing = false;
+    attackCharge = 0;
 }
-
+document.getElementById("adv").addEventListener("click", function(d){
+    advancing = true;
+})
 
 var levelFactor = 1;
 function levelUp(){
     if(exp >= expToNext){
         exp = exp - expToNext;
         level++;
-        expToNext = Math.floor(3.871 * Math.pow(1.5 + (0.05 * level), level))
+        expToNext = 6 * (Math.pow(1.5, level))
         document.getElementById("leveltext").innerHTML = "Level " + level;
         playerLoadHp(0.15);
     }
@@ -91,6 +118,9 @@ function playerLoadHp(restore){
     levelFactor = Math.pow(1.15, level - 1)
     playerHp = (50 * levelFactor)
     playerCurrentHp += playerHp * restore;
+    if(playerCurrentHp > playerHp){
+        playerCurrentHp = playerHp;
+    }
 }
 
 document.getElementById("treepanel").style.display = "none"
@@ -108,19 +138,30 @@ function Attack(){
     enemyHp -= AD
     if(enemyHp <= 0){kill()}
 }
+function valueCalc(){
+    value = Math.floor((2 * Math.pow(1.25, zone - 1)))
+    value *= ((Math.floor(zone / 10))+1)
+}
 function kill(){
-    gold  += Math.floor((2 * Math.pow(1.2, zone - 1)))
+    valueCalc()
+    gold  += value
+    if(zone == hZE && Math.floor(zone/10) == zone/10){tomes += 1;}
     enemyCharge = 0;
-    playerLoadHp(0.15);
+    playerLoadHp(1);
     if(playerCurrentHp > playerHp){playerCurrentHp = playerHp}
     exp += (Math.pow(1.4, zone-1))
     levelUp();
-    loadZone(1)
+    if(advancing == true){loadZone(1)} else {loadZone(0)}
+    attackCharge = 0;
 }
 document.getElementById("sword").addEventListener("click", function(buyS){
     buy("sword")
 })
+document.getElementById("bigsword").addEventListener("click", function(buyBS){
+    buy("bigsword")
+})
 ADPurchase = 0;
+AD2Purchase = 0;
 function buy(thing){
     if(thing == "sword"){
         if(gold >= Math.floor(4 * (Math.pow(1.5, ADPurchase)))){
@@ -130,9 +171,19 @@ function buy(thing){
             document.getElementById("swordtext").innerHTML = "Sword: Level "+ ADPurchase;
         }
     }
+    if(thing == "bigsword"){
+        if(gold >= Math.floor(50 * Math.floor(Math.pow(10, AD2Purchase)))){
+            gold -= Math.floor(50 * Math.floor(Math.pow(10, AD2Purchase)));
+            AD2Purchase += 1;
+            document.getElementById("bigsword").innerHTML = "Cost: " + Math.floor(50 * Math.floor(Math.pow(10, AD2Purchase)))
+
+        }
+    }
 }
 function ADCalc(){
     AD = 1 + ADPurchase;
+    AD = AD * Math.pow(2, AD2Purchase)
+    document.getElementById("ad").innerHTML = AD + " AD"
 }
 playerHpBar = document.getElementById("playerhpbar");
 hpBar = document.getElementById("enemyhpbar")
@@ -147,4 +198,7 @@ function barUpdate(){
     expBar.innerHTML = Math.floor((exp / expToNext) * 100) + "%"
     attackBar.style.width = (400 * (attackCharge / attackSeconds)) + "px"
     attackBar.innerHTML = Math.floor(attackCharge * 100 / attackSeconds) + "%"
+    if(advancing == true){
+        document.getElementById("adv").style.display = "none";
+    }else{document.getElementById("adv").style.display = "inline-block";}
     }
